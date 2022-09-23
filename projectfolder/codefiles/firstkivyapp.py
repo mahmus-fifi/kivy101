0f15a45216a1c675e47ac694914efa5d1b1cfa38
mahmus-fifi <mahmudshuaib555@gmail.com>
2022-09-23 07:35:25 +0100

    created a hello world application with kivy

--- projectfolder/codefiles/firstkivyapp.py
+++ projectfolder/codefiles/firstkivyapp.py
@@ -1,4 +1,17 @@
+#
 import kivy 
-print('hi mama')
-message = 'hi mama'
-print('Greetings: %s'%message)
+kivy.require("2.1.0")
+from kivy.app import App
+from kivy.uix.label import Label
+"""print('hi mama')
+print('Greetings: %s'%message)"""
+message = 'Hello Mimi Dinosaur!!!! RoaaaaaRRRRRRRRRRR'
+# creating a class to inherit from kivy base class 
+class MlxApp(App):
+    def build(self):
+        return Label(text = message)
+    
+if __name__ == '__main__':
+    MlxApp().run()
+    
+    
